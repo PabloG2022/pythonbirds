0b2e3152489fbbd28e98eafbbb9556fc0d50a74f
PabloG2022 <pabloguilherme90@hotmail.com>
2022-09-21 12:33:42 -0300

    Implementada direção e motor

--- oo/carro.py
+++ oo/carro.py
@@ -88,6 +88,29 @@ seguintes atribus
     >>> carro.calcular_direcao()
     'Oeste'
 """
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.velocidade()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        def.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        def .direcao.girar_a_esquerda()
+
 
 NORTE='Norte'
 SUL ='Sul'
